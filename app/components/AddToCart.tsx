e05d0e06b9930e4e2fa4957cc90d5c5bd117a950
shrewman <feelswhiteman@gmail.com>
2024-07-19 11:47:40 +0300

    create server ProductCard component and client AddToCart component

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const AddToCartButton = () => {
+  return <button onClick={() => console.log("add")}>add to cart</button>;
+};
+
+export default AddToCartButton;
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AddToCartButton from "./AddToCart";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <h1>ProductCard</h1>
+      <AddToCartButton />
+    </div>
+  );
+};
+
+export default ProductCard;
