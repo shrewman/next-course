9c0b25106ca20255d9e23f6336e4b667e50fed4a
shrewman <feelswhiteman@gmail.com>
2024-07-20 20:45:07 +0300

    make programmatic navigation from /users/new to /users with UseRouter()

---
+++ app/users/new/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import React from 'react'
+
+const NewUserPage = () => {
+  const router = useRouter();
+  return (
+    <button onClick={() => router.push("/users")} className='btn btn-primary'>CREATE</button>
+  )
+}
+
+export default NewUserPage
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import UserTable from "./UserTable";
+import Link from "next/link";
 
 type Props = {
   searchParams: { sortOrder: keyof User }
@@ -9,6 +10,7 @@ const UsersPage = async ({ searchParams }: Props) => {
   return (
     <>
       <h1 className="text-2xl font-bold mb-5">UsersPage</h1>
+      <Link href="/users/new" className="btn btn-sm">New user</Link>
       <UserTable sortOrder={searchParams.sortOrder} />
     </>
   );
