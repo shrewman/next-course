1cd4a083735c573c8513866dd23e2b70a1641339
shrewman <feelswhiteman@gmail.com>
2024-07-21 08:57:40 +0300

    display loading table while data is being fetched in UsersPage

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { Suspense } from "react";
 import UserTable from "./UserTable";
 import Link from "next/link";
 
 type Props = {
-  searchParams: { sortOrder: keyof User }
-}
+  searchParams: { sortOrder: keyof User };
+};
 
 const UsersPage = async ({ searchParams }: Props) => {
   return (
     <>
       <h1 className="text-2xl font-bold mb-5">UsersPage</h1>
-      <Link href="/users/new" className="btn btn-sm">New user</Link>
-      <UserTable sortOrder={searchParams.sortOrder} />
+      <Link href="/users/new" className="btn btn-sm">
+        New user
+      </Link>
+      <Suspense fallback={<LoadingTable />}>
+        <UserTable sortOrder={searchParams.sortOrder} />
+      </Suspense>
     </>
   );
 };
 
 export default UsersPage;
+
+const LoadingTable = async () => {
+  return (
+    <>
+      <h1 className="text-2xl font-bold mb-5">UsersPage</h1>
+      <table className="table table-xs">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {[1, 2, 3, 4].map((n) => (
+            <tr key={n}>
+              <td>...</td>
+              <td>...</td>
+              <td>...</td>
+              <td>...</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
