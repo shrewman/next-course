a1839ae161d43e28c6d15776da6e25158c2edf07
shrewman <feelswhiteman@gmail.com>
2024-07-21 08:56:28 +0300

    show the spinner animation for all loading pages

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="absolute left-1/2 top-1/2">
+      <span className="loading loading-spinner loading-lg"></span>
+    </div>
+  );
+};
+
+export default Loading;
