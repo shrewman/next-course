f68ecadea0289a44caf6e7fe6ba885f13951bec3
shrewman <feelswhiteman@gmail.com>
2024-07-25 13:08:41 +0300

    define PUT method for updating the user object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -14,3 +14,26 @@ export async function GET(request: NextRequest, { params }: Props) {
   }
   return NextResponse.json(user);
 }
+
+export async function PUT(request: NextRequest, { params }: Props) {
+  const body = await request.json();
+  const { username, email, phone } = body;
+
+  if (!username)
+    return NextResponse.json(
+      { error: "The username is required" },
+      { status: 400 },
+    );
+
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${params.id}`,
+  );
+  const user = await res.json();
+
+  if (!user.id) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+
+  return NextResponse.json({ id: params.id, username, email, phone });
+}
+
