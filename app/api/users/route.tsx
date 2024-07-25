585b180b41bf1f069ad74b95377aff7d4957b862
shrewman <feelswhiteman@gmail.com>
2024-07-25 13:10:29 +0300

    define POST method for creating the user object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -5,3 +5,21 @@ export async function GET(request: NextRequest) {
   const users = await res.json();
   return NextResponse.json(users);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  if (!body.username)
+    return NextResponse.json(
+      { error: "The username is required" },
+      { status: 400 },
+    );
+  return NextResponse.json(
+    {
+      id: 1,
+      username: body.username,
+      email: body.email,
+      phone: body.phone,
+    },
+    { status: 201 },
+  );
+}
