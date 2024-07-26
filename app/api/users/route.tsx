d74c19a9c978d0325b5894f9d9e6351a4f54f9b4
shrewman <feelswhiteman@gmail.com>
2024-07-26 15:22:03 +0300

    insert User to database in POST method

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -10,15 +10,23 @@ export async function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = schema.safeParse(body);
-  if (!validation.success)
+  if (!validation.success) {
     return NextResponse.json(validation.error.errors, { status: 400 });
-  return NextResponse.json(
-    {
-      id: 1,
-      username: body.username,
-      email: body.email,
-      phone: body.phone,
-    },
-    { status: 201 },
-  );
+  }
+
+  const { name, username, email, phone } = body;
+
+  const user = await prisma.user.findFirst({
+    where: { OR: [{ email }, { username }] },
+  });
+
+  if (user) {
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+  }
+
+  const newUser = await prisma.user.create({
+    data: { name, username, email, phone },
+  });
+
+  return NextResponse.json(newUser, { status: 201 });
 }
