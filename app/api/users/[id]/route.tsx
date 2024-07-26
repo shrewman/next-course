2dca03644c91681bb632b60c639227229c9f6dd5
shrewman <feelswhiteman@gmail.com>
2024-07-26 18:57:08 +0300

    PUT and POST methods work with database now

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -10,39 +10,44 @@ export async function GET(request: NextRequest, { params }: Props) {
   const user = await prisma.user.findUnique({
     where: { id: parseInt(params.id) },
   });
+
   if (!user) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   }
+
   return NextResponse.json(user);
 }
 
 export async function PUT(request: NextRequest, { params }: Props) {
   const body = await request.json();
-  const { username, email, phone } = body;
+  const { name, username, email, phone } = body;
+  const id = parseInt(params.id);
 
   const validation = schema.safeParse(body);
-  if (!validation.success)
+  if (!validation.success) {
     return NextResponse.json(validation.error.errors, { status: 400 });
+  }
 
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/users/${params.id}`,
-  );
-  const user = await res.json();
-
-  if (!user.id) {
+  const targetUser = await prisma.user.findUnique({ where: { id } });
+  if (!targetUser) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   }
 
-  return NextResponse.json({ id: params.id, username, email, phone });
+  const updatedUser = await prisma.user.update({
+    where: { id },
+    data: { name, username, email, phone },
+  });
+  return NextResponse.json(updatedUser);
 }
 
 export async function DELETE(request: NextRequest, { params }: Props) {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/users/${params.id}`,
-  );
-  const user = await res.json();
-  if (!user.id) {
+  const id = parseInt(params.id);
+
+  const targetUser = await prisma.user.findUnique({ where: { id } });
+  if (!targetUser) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   }
+
+  await prisma.user.delete({ where: { id } });
   return NextResponse.json({});
 }
