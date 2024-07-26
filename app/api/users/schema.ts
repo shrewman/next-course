f84cbcefa5f5f101265047174a6e684d23075e59
shrewman <feelswhiteman@gmail.com>
2024-07-26 15:21:42 +0300

    add fields to User model, update schema

--- app/api/users/schema.ts
+++ app/api/users/schema.ts
@@ -1,9 +1,10 @@
 import { z } from "zod";
 
 const schema = z.object({
+  name: z.string().min(3).optional(),
   username: z.string().min(3),
   email: z.string().email(),
-  phone: z.string().min(10)
+  phone: z.string().min(10).optional(),
 })
 
 export default schema
---
+++ prisma/migrations/20240726114417_add_username/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `User` ADD COLUMN `username` VARCHAR(191) NULL,
+    MODIFY `name` VARCHAR(191) NULL;
